9927bc6dbb658c6bf63b4c13160298a44456a997
gideonshmila <gideons@metisdata.io>
2023-04-11 10:10:14 +0300

    p

--- .github/actions/bump-version/index.js
+++ .github/actions/bump-version/index.js
@@ -1,14 +1,15 @@
-const core = require("@actions/core");
+const core = require('@actions/core')
 
 async function main() {
-  const lastTag = core.getInput("tag");
-  core.info(lastTag);
-  let version = lastTag.replace("v", "");
-  core.info(version);
-  version = version.split(".").map((i) => Number.parseInt(i));
-  version[2]++;
+  const lastTag = core.getInput('tag')
+  core.info('hellooo!!!')
+  core.info(lastTag)
+  let version = lastTag.replace('v', '')
+  core.info(version)
+  version = version.split('.').map(i => Number.parseInt(i))
+  version[2]++
 
-  core.setOutput("tag", `v${version.join(".")}`);
+  core.setOutput('tag', `v${version.join('.')}`)
 }
 
-main();
+main()
