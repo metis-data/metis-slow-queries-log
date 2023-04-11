62ce734479cac640575416282565f3cd2f8aca9c
gideonshmila <gideons@metisdata.io>
2023-04-11 10:08:14 +0300

    p

--- .github/actions/bump-version/index.js
+++ .github/actions/bump-version/index.js
@@ -2,8 +2,9 @@ const core = require("@actions/core");
 
 async function main() {
   const lastTag = core.getInput("tag");
-
+  core.info(lastTag);
   let version = lastTag.replace("v", "");
+  core.info(version);
   version = version.split(".").map((i) => Number.parseInt(i));
   version[2]++;
 
