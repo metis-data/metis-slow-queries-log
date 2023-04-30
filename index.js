7cc85e56757408e56cc267412ad43c6f6f19b328
gideonshmila <gideons@metisdata.io>
2023-04-30 11:17:02 +0300

    p

--- index.js
+++ index.js
@@ -47,7 +47,7 @@ async function run() {
     console.log('check');
     core.info('check -info');
     console.log(`log: ${slowQueryLogData}`);
-    core.info(`info: ${slowQueryLogData}`);
+    core.info(`info: ${JSON.stringify(slowQueryLogData)}`);
     await sendSpansFromSlowQueryLog(metisApikey, core.getInput('metis_api_key'), core.getInput('metis_exporter_url'), slowQueryLogData, dbConnection, core.getInput('target_url'));
   } catch (error) {
     console.error(error);
